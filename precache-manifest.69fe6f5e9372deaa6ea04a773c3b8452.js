abce3dbdfd11b5806ec365df9848ea85c382696f
Aditya2202 <aditya4bel@gmail.com>
2022-07-23 01:16:11 +0530

    deploying to special repo

---
+++ precache-manifest.69fe6f5e9372deaa6ea04a773c3b8452.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7ff312240b27e84a76a9f639660b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80055965aef5d3b9f9a5",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a02420ded0e19f1ba05b",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "972f6f1190c0c7a815ab",
+    "url": "/static/css/main.9a83d4e7.chunk.css"
+  },
+  {
+    "revision": "dc0aa5c20947052ac7e7",
+    "url": "/static/js/2.3c0dd12b.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.3c0dd12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80055965aef5d3b9f9a5",
+    "url": "/static/js/3.81c4ccd6.chunk.js"
+  },
+  {
+    "revision": "a02420ded0e19f1ba05b",
+    "url": "/static/js/4.430937f6.chunk.js"
+  },
+  {
+    "revision": "972f6f1190c0c7a815ab",
+    "url": "/static/js/main.de72bf6b.chunk.js"
+  },
+  {
+    "revision": "49ccb3fc72ff25a4c848",
+    "url": "/static/js/runtime-main.ef770ca9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "da07f41a7bf9dffc2d5d3dffee3b480a",
+    "url": "/static/media/africa_sunset.da07f41a.jpg"
+  },
+  {
+    "revision": "a7b7048f8e85c56340e9230117093b64",
+    "url": "/static/media/cocktail.a7b7048f.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "6d58d071c7a937cef029507480f9a150",
+    "url": "/static/media/dav_logo.6d58d071.jpeg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "253f7fe76e088815b8e713f6556b3ef2",
+    "url": "/static/media/mushroom.253f7fe7.jpg"
+  },
+  {
+    "revision": "9819e08bb114b997a1e17724f8262042",
+    "url": "/static/media/mvit_logo.9819e08b.jpeg"
+  },
+  {
+    "revision": "6072e3e00d417659982e42b4401a12c6",
+    "url": "/static/media/retro.6072e3e0.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "472ca78da86120281585edf7381483ee",
+    "url": "/static/media/snake.472ca78d.jpg"
+  },
+  {
+    "revision": "3bf90f1fb847a3748a8b43cae2bff1d0",
+    "url": "/static/media/spider.3bf90f1f.jpg"
+  },
+  {
+    "revision": "359060fa70323c5cbc6f28988fa34f27",
+    "url": "/static/media/stepbystep_logo.359060fa.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
